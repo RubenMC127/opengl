99f8fca39bf145825b386f7e45fdfeaa346cd531
rubenmc127 <rubenmcampos@gmail.com>
2023-02-25 13:02:12 +0100

    First Triangle! Also: Update include + opengl version usage

--- triangle/src/main.cpp
+++ triangle/src/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include "glad.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
 
 void processInput(GLFWwindow* window)
 {
@@ -16,8 +17,12 @@ GLFWwindow* initialize(int width, int height)
     if (!glfwInit())
         return NULL;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Window!", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(width, height, "Window!", NULL, NULL);
     if (!window)
     {
         return NULL;
@@ -33,7 +38,8 @@ GLFWwindow* initialize(int width, int height)
         return NULL;
     }
 
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    glViewport(0, 0, width, height);
+
     return window;
 }
 
@@ -108,22 +114,60 @@ int main(void)
     unsigned int program = createProgram();
     glUseProgram(program);
 
+    // data format
+    // x,y,z,r,g,b
+    std::vector<float> vertices = { {
+        -0.25f, -0.25f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.25f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.25f, -0.25f, 0.0f, 0.0f, 0.0f, 1.0f,
+    } };
+    int vertexCount = 3;
+
+    unsigned int VBO;
+    glCreateBuffers(1, &VBO);
+
+    unsigned int VAO;
+    glCreateVertexArrays(1, &VAO);
+    // Binding VBO to index 0 of VAO
+    glVertexArrayVertexBuffer(VAO, 0, VBO, 0, 6*sizeof(float));
+    // Assigning vertices array to VBO
+    glNamedBufferStorage(VBO, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_STORAGE_BIT);
+
+    // Enable and defining VAO[0] attributes
+    // Defining Position Attribute at index 0 of attrib
+    glEnableVertexArrayAttrib(VAO, 0);
+    glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
+    glVertexArrayAttribBinding(VAO, 0, 0);
+
+    // Defining Colot Attribute at index 1 of attrib
+    glEnableVertexArrayAttrib(VAO, 1);
+    glVertexArrayAttribFormat(VAO, 1, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float)); // 3 last floats of the vertex
+    glVertexArrayAttribBinding(VAO, 1, 0);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
         /* Handles key input events */
         processInput(window);
+        /* Poll for and process events */
+        glfwPollEvents();
+        /* Render here */
+        glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Binding Shader Program
+        glUseProgram(program);
+        // Binding VAO & Drawing!
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, vertexCount);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
     }
 
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
     glDeleteProgram(program);
     glfwTerminate();
     return 0;
